83f57565cb28cd70a9991181c343a44562472a04
Muzammil Osman <usman.muzammil@gmail.com>
2022-04-08 18:04:50 +0530

    lint changes

--- components/newsletter.js
+++ components/newsletter.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import { postAPI } from '../lib/api';
+import React from "react"
+import { postAPI } from "../lib/api"
 
 export const Newsletter = () => {
-
   const submitEmail = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
     const newsletterEmail = event.target.email.value
-    const emailResponse = await postAPI('/newsletters', {email: newsletterEmail});
+    const emailResponse = await postAPI("/newsletters", {
+      email: newsletterEmail,
+    })
     console.log(emailResponse)
   }
 
   return (
     <div>
-        <form className='row' onSubmit={submitEmail}>
-            <input placeholder='Enter your email to subscribe for our newsletter' name='email' className='col-md-10' type="email" />
-            <button className='btn col-md-2' type="submit">SUBSCRIBE</button>
-        </form>
+      <form className="row" onSubmit={submitEmail}>
+        <input
+          placeholder="Enter your email to subscribe for our newsletter"
+          name="email"
+          className="col-md-10"
+          type="email"
+        />
+        <button className="btn col-md-2" type="submit">
+          SUBSCRIBE
+        </button>
+      </form>
     </div>
   )
 }
